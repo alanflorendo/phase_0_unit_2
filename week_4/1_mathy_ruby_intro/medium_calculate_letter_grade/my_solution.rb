71d17b81ee5a54f78abc5df3f17ad5e1f2f9151e
Alan Florendo <alan.florendo@gmail.com>
2014-05-12 16:01:30 -0500

    4.1 letter grade, initial solution

--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -26,6 +26,31 @@
 
 
 # 2. Initial Solution
+# Create get_grade method with a single argument (grades_array)
+def get_grade(grades_array)
+	# Determine the average of the grades
+		# Set a new variable called grades_total to zero
+		grades_total = 0
+		# Add each grade in grades_array to grades_total
+		grades_array.each do |x|
+			grades_total += x
+		end
+		# New variable average = grades_total divided by the array-length of grades_array		
+		average_number = grades_total / grades_array.length
+	# Use case statement to convert the average to a letter grade
+	case average_number
+	when 0...60
+		return "F"
+	when 60...70
+		return "D"
+	when 70...80
+		return "C"
+	when 80...90
+		return "B"
+	when 90...100
+		return "A"
+	end
+end
 
 
 
