f98b5350567991b3f9d6406b6b25988322b18ff7
Alan Florendo <alan.florendo@gmail.com>
2014-05-12 17:11:34 -0500

    4.1 mode, refactoring and reflection

--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -25,7 +25,7 @@
 	# return mode_array
 
 # 2. Initial Solution
-
+=begin
 # create a new method called mode, with single argument (input_array)
 def mode(input_array)
 	# create a hash variable called occurrences, with a default value of zero
@@ -56,10 +56,46 @@ def mode(input_array)
 	# return mode_array
 	return mode_array
 end
-
+=end
 
 # 3. Refactored Solution
-
+# create a new method called mode, with single argument (input_array)
+def mode(input_array)
+	# create a hash variable called occurrences, with a default value of zero
+	occurrences = Hash.new(0)
+	# for each number in input_array, increment the value for that number key by 1
+	input_array.each do |x|
+		occurrences[x] += 1
+	end
+	# CREATE A NEW VARIABLE CALLED MAX_NUM_OCCURRENCES, EQUAL TO THE MOST TIMES ANY NUMBER(S) APPEAR(S)
+	max_num_occurrences = occurrences.values.max
+	
+	# create a new variable mode_array
+	mode_array = []
+	counter = 0
+	# for each pairing in the hash, if the hash-value equals max_num_occurrences, add its key to hash_value_array
+	occurrences.each do |key, value|
+		if value == max_num_occurrences
+			mode_array[counter] = key
+			counter += 1
+		end
+	end
+	# return mode_array
+	return mode_array
+end
 
 
 # 4. Reflection 
+=begin
+
+This challenge was significantly harder than the prior two, and I am proud I was able to complete both my initial solution
+and my refactoring (brief as it was).  I initially considered not refactoring based on the idea that my Ruby was not strong 
+enough.  A quick google search, however, got me the easy syntax I should have looked for in my initial solution.  That's
+lesson #1:  google is an amazing resource for syntax (presumably for simple as well as complex, though that may be harder
+to find).
+
+I also used IRB as a debugging tool.  Though I am sure there are better debuggers, IRB was pretty effective for code this
+simple.  I had not used IRB in the prior challenges this week, so this definitely put that tool back on my radar.  That's
+lesson #2.
+
+=end
