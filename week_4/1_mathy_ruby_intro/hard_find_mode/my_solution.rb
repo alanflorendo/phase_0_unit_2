0457dbb5c3e6d1faa8350c1ea6b0f57ada73458f
Alan Florendo <alan.florendo@gmail.com>
2014-05-12 16:52:45 -0500

    4.1 mode, initial solution

--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -16,15 +16,46 @@
 
 # What are the steps needed to solve the problem?
 # create a new method called mode, with single argument (input_array)
-# create a hash variable called occurrences, with a default value of zero
-# for each number in input_array, increment the value for that number key by 1
-# create an array of all values from the hash (hash_value_array)
-# create a variable num_mode_occurrences equal to the maximum of hash_value_array
-# for each pairing in the hash, if the hash-value equals num_mode_occurrences, add its key to hash_value_array
+	# create a hash variable called occurrences, with a default value of zero
+	# for each number in input_array, increment the value for that number key by 1
+	# create a new variable called max_num_occurrences, equal to the occurrences of the first array value (default)
+	# for each hash value, compare the hash value to max_num_occurrences, replacing if the hash value is greater
+	# create a new variable mode_array
+	# for each pairing in the hash, if the hash-value equals max_num_occurrences, add its key to hash_value_array
+	# return mode_array
 
 # 2. Initial Solution
 
+# create a new method called mode, with single argument (input_array)
+def mode(input_array)
+	# create a hash variable called occurrences, with a default value of zero
+	occurrences = Hash.new(0)
+	# for each number in input_array, increment the value for that number key by 1
+	input_array.each do |x|
+		occurrences[x] += 1
+	end
 
+	# create a new variable called max_num_occurences, equal to the occurrences of the first array value (default) 
+	max_num_occurrences = occurrences[input_array[0]]
+	# for each hash value, compare the hash value to max_num_occurrences, replacing if the hash value is greater
+	occurrences.each do |key, value|
+		if value > max_num_occurrences
+			max_num_occurrences = value
+		end
+	end
+	# create a new variable mode_array
+	mode_array = []
+	counter = 0
+	# for each pairing in the hash, if the hash-value equals max_num_occurrences, add its key to hash_value_array
+	occurrences.each do |key, value|
+		if value == max_num_occurrences
+			mode_array[counter] = key
+			counter += 1
+		end
+	end
+	# return mode_array
+	return mode_array
+end
 
 
 # 3. Refactored Solution
