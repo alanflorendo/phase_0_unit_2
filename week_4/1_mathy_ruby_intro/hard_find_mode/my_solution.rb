587a8380ec1cd5d6485028ae7c7d4fe7366b47ea
Alan Florendo <alan.florendo@gmail.com>
2014-05-12 16:15:49 -0500

    4.1 mode, pseudocode

--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -4,14 +4,23 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
 # What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
+# input is an array of numbers
 
+# What is the output? (i.e. What should the code return?)
+# output is an array of mode(s) (the number(s) occurring most often in the input array)
+
+# What are the steps needed to solve the problem?
+# create a new method called mode, with single argument (input_array)
+# create a hash variable called occurrences, with a default value of zero
+# for each number in input_array, increment the value for that number key by 1
+# create an array of all values from the hash (hash_value_array)
+# create a variable num_mode_occurrences equal to the maximum of hash_value_array
+# for each pairing in the hash, if the hash-value equals num_mode_occurrences, add its key to hash_value_array
 
 # 2. Initial Solution
 
