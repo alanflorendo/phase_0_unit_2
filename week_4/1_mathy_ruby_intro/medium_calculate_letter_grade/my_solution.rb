9274e115a437be37ed8150da2a968555c28dcd1e
Alan Florendo <alan.florendo@gmail.com>
2014-05-12 15:50:38 -0500

    4.1 letter grade, pseudocode

--- week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
+++ week_4/1_mathy_ruby_intro/medium_calculate_letter_grade/my_solution.rb
@@ -4,17 +4,28 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 1. Pseudocode
 
 # What is the input?
+# an array of numeric grades
+
 # What is the output? (i.e. What should the code return?)
+# a single letter grade
+
 # What are the steps needed to solve the problem?
+
+# Create get_grade method with a single argument (grades_array)
+	# Determine the average of the grades
+		# Set a new variable called grades_total to zero
+		# Add each grade in grades_array to grades_total
+		# New variable average = grades_total divided by the array-length of grades_array
+	# Use case statement to convert the average to a letter grade
+
 
 
 # 2. Initial Solution
-
 
 
 
