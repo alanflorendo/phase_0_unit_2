6c163ba0867d0f93c85e1b97eaf3b907ad6d1818
Alan Florendo <alan.florendo@gmail.com>
2014-05-12 12:01:36 -0500

    4.1:  updated pseudocode

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -7,6 +7,16 @@
 # I worked on this challenge [by myself, with: ].
 
 # 1. Pseudocode
+
+# What is the input?
+# For each of the two functions the input is an array: an array of numbers for the total method and
+# an array of strings for the sentence_maker method.
+
+# What is the output? (i.e. What should the code return?)
+# The total method will return a sum of the members of the inputted array.
+# The sentence_maker method will return a concatenated sentence, made of the members of the inputted array.
+
+# What are the steps needed to solve the problem?
 # Create total method with single argument: array_to_total
 	# Declare new sum variable and set to zero
 	# For each member of array_to_total, add to the sum variable
@@ -15,13 +25,11 @@
 # Create sentence_maker method with single argument: array_of_strings
 	# Declare new sentence variable and set to ""
 	# For each member of array_of_strings, add to the sentence variable
+		# if the member of the array is the first member of array_of_strings, capitalize it
+	# End the sentence variable with a period
 	# Return the sentence variable
 
 
-
-# What is the input?
-# What is the output? (i.e. What should the code return?)
-# What are the steps needed to solve the problem?
 
 
 # 2. Initial Solution
