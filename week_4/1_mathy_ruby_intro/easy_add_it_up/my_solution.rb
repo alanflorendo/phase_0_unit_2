fec96441a5043df9ef6c1b63308f2f14041c0bbd
Alan Florendo <alan.florendo@gmail.com>
2014-05-12 11:57:11 -0500

    4.1: pseudocode

--- week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
+++ week_4/1_mathy_ruby_intro/easy_add_it_up/my_solution.rb
@@ -7,6 +7,17 @@
 # I worked on this challenge [by myself, with: ].
 
 # 1. Pseudocode
+# Create total method with single argument: array_to_total
+	# Declare new sum variable and set to zero
+	# For each member of array_to_total, add to the sum variable
+	# Return the sum variable
+
+# Create sentence_maker method with single argument: array_of_strings
+	# Declare new sentence variable and set to ""
+	# For each member of array_of_strings, add to the sentence variable
+	# Return the sentence variable
+
+
 
 # What is the input?
 # What is the output? (i.e. What should the code return?)
