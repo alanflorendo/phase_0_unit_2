f0100941243494673e553e093db04c4a4f80a8d6
Alan Florendo <alan.florendo@gmail.com>
2014-05-18 16:32:35 -0500

    4.2 driver code

--- week_4/2_creative/create_accountability_groups/my_solution.rb
+++ week_4/2_creative/create_accountability_groups/my_solution.rb
@@ -1,12 +1,12 @@
 # U2.W4: Create Accountability Groups
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 # 2. Pseudocode
 
-# Input:
-# Output:
+# Input:  a list of students
+# Output:  a hash of those students with accountability groups
 # Steps:
 
 
@@ -25,6 +25,26 @@
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+students = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
+	"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+puts "Test 1:  Test Group 1 for minimum size of 4"
+puts "#{hash_value_min(new_ag_bracket(students)) >= 4}"
+
+puts "Test 2:  Test Group 2 for minimum size of 4"
+puts "#{hash_value_min(new_ag_bracket(students)) >= 4}"
+
+puts "Test 3:  Test Group 3 for minimum size of 4"
+puts "#{hash_value_min(new_ag_bracket(students)) >= 4}"
+
+puts "Test 1:  Test Group 1 for maximum size of 6"
+puts "#{hash_value_min(new_ag_bracket(students)) <= 6}"
+
+puts "Test 2:  Test Group 2 for maximum size of 6"
+puts "#{hash_value_min(new_ag_bracket(students)) <= 6}"
+
+puts "Test 3:  Test Group 3 for maximum size of 6"
+puts "#{hash_value_min(new_ag_bracket(students)) <= 6}"
 
 
 
