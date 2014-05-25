1da0d07b4eeef57fd8f1b139e235732a5dde56f9
Alan Florendo <alan.florendo@gmail.com>
2014-05-25 17:32:08 -0500

    5.6 and 5.9

--- week_5/6_validate_credit_card/my_solution.rb
+++ week_5/6_validate_credit_card/my_solution.rb
@@ -1,37 +1,143 @@
 # U2.W5: Class Warfare, Validate a Credit Card Number
 
 
-# I worked on this challenge [by myself, with: ].
+# I (Alan Florendo) worked on this challenge Farheem Malik.
 
 # 2. Pseudocode
 
 # Input:
+# Credit Card with Number
+
 # Output:
+# Whether the card number is valid or invalid
+
 # Steps:
+# Create a class CreditCard 
+  # Initialize card with 16-digit card number
+  # Raise error if card is not 16 digits
+  
+  # Step 1: create double_element method
+    # Step 1a: Convert credit card number string to array of characters
+    # Step 1b: Double every even numbered element
+  # Step 2: create sum_elements method
+    # Step 2a:  If any element has two digits, add them together (12 = 1 + 2)
+    # Step 2b: Sum all elements together
+  # Step 3: Create check_card method. 
+    # Return true if sum is a multiple of 10 (%10), otherwise false
 
 
 # 3. Initial Solution
 
+=begin
 # Don't forget to check on intialization for a card length
 # of exactly 16 digits
 class CreditCard
+  def initialize(card_num)
+      if card_num.to_s.length == 16
+         @card_num = card_num
+      else
+          raise ArgumentError.new("Card must be 16 digits")
+      end
+  end
+  
+  def check_card
+      @card_num = @card_num.to_s.split('').map(&:to_i)
+      16.times do |index|
+          if index % 2 != 0
+              @card_num[index] *= 2
+          end
+      end
+      
+      card_index = 0
+      @card_num.each do |element|
+          if element > 9
+              dummy = element.to_s.split('').map(&:to_i)
+              element = dummy[0] + dummy[1]
+              @card_num[card_index] = element
 
+          end
+         card_index += 1
+      end
+      
+      digits_sum = 0
+      @card_num.each do |element|
+          digits_sum += element.to_i
+      end
+      
+      if digits_sum % 10 == 0
+          true
+      else
+          false
+      end
+      
+          
+  end
+      
 end
 
+=end
 
 
 # 4. Refactored Solution
-
+class CreditCard
+  def initialize(card_num)
+      @card_num = card_num
+      raise ArgumentError.new("Card must be 16 digits") if card_num.to_s.length != 16
+  end
+  
+  def check_card
+      @card_num = @card_num.to_s.split('').map(&:to_i)
+      16.times do |index|
+          @card_num[index] *= 2 if index % 2 != 0
+      end
+      
+      @digits_to_sum = []
+      @card_num.each do |element|
+          if element > 9
+              @digits_to_sum << 1 + (element % 10)
+          else
+              @digits_to_sum << element
+          end
+      end
+              
+      digits_sum = 0
+      @digits_to_sum.each do |element|
+          digits_sum += element
+      end
+      
+      digits_sum % 10 == 0
+   end
+end
 
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
+valid_card = CreditCard.new(4408041234567893)
+puts valid_card.check_card == true
 
+invalid_card = CreditCard.new(4408041234567892)
+puts invalid_card.check_card == false
 
-
-
+# error_card = CreditCard.new(1234) # => raises ArgumentError
 
 
 # 5. Reflection 
+
+=begin
+We did well to focus first on leveraging techniques we knew we could do in order to solve the problem, 
+which we both found complex from the beginning.  We pretty much embraced some sloppy loops and if
+statements, as long as we both understood what the blocks were trying to do.  We had several issues
+converting numbers to strings and back again, so we used trial and error to figure out where the 
+problems were, probably adding several unnecessary "to_i"s and "to_s"s.  We used both a debugger
+and internal puts/print statements to figure out where our code was breaking down.
+
+Once we got our code to solve the problem, we used the refactoring as an opportunity to refine our
+code and to research other methods we were less familiar with (like mapping).  The result is certainly
+a much shorter and hopefully cleaner code.  I think it reads for a more sophisticated audience--I 
+do no know that I would necessarily be understand what we wrote if I saw it cold.  But I do think
+it is in line with what other coders are doing, and bringing it to this more sophisticated (higher-
+level) style will definitely help us get better at reading others' code as well.
+
+=end
--- week_5/9_gps2.1/my_solution.rb
+++ week_5/9_gps2.1/my_solution.rb
@@ -1,6 +1,6 @@
 # U2.W5: Bakery Challenge GPS
 
-# I worked on this challenge with: 
+# I (Alan Florendo) worked on this challenge with Daniel Deepak: 
 
 
 
@@ -9,6 +9,40 @@
 
 # Our Refactored Solution
 
+def bakery_num(num_people, fav_food)
+  # create hash of baked goods and # of people it will serve
+  food_servings = { "pie" => 8, "cake" => 6, "cookie" => 1 }
+               
+  # return error message if invalid food is requested
+  raise ArgumentError.new("You can't make that food") if food_servings.include?(fav_food) == false
+
+  # if no error proceed to determine number of orders
+  
+    # if fave food evenly serves number of people entered,
+    # use simple integer division to calculate qty of food
+    if num_people % food_servings[fav_food] == 0
+     return "You need to make #{num_people / food_servings[fav_food]} #{fav_food}(s)."
+    end 
+
+    # if fave food does not serve number of people equally,
+    # then determine number of people it can serve and what other foods
+    # need to be made to serve the remainder of people
+
+    # create a new hash with foods as keys and quantity to be made as values
+    food_orders = Hash.new(0)
+    # first, give a round number of people their favorite food
+    food_orders[fav_food] += num_people / food_servings[fav_food] #(integer division)
+    num_people = num_people % food_servings[fav_food] 
+    # second, give the remainder of people whatever--who cares if it's not what they want to eat!?
+
+    # loop thru hash to determine qtys of each non-fave food (reducing num_people each time)
+      food_servings.each do |key, value|
+        food_orders[key] += num_people / value
+        num_people = num_people % value
+      end 
+
+    return "You need to make #{food_orders["pie"]} pie(s), #{food_orders["cake"]} cake(s), and #{food_orders["cookie"]} cookie(s)."
+end 
 
 
 
@@ -28,12 +62,33 @@ p bakery_num(130, "pie") == "You need to make 16 pie(s), 0 cake(s), and 2 cookie
 # p bakery_num(3, "apples") # this will raise an ArgumentError
 
 # You SHOULD change this driver code. Why? Because it doesn't make sense.
-p bakery_num(41, "cake") == "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
+#p bakery_num(41, "cake") #== "You need to make 5 pie(s), 0 cake(s), and 1 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
+# This should be
+p bakery_num(41, "cake") == "You need to make 0 pie(s), 6 cake(s), and 5 cookie(s)." # WHAAAAAT? I thought I said I wanted cake!
 
 
 
 
 #  Reflection 
+=begin
+This exercise definitely taught us several lessons:  (1) reading code written by others is a different 
+exercise than writing your own code; (2) proper indentation is an absolute necessity for code to be well
+understood by humans (even if not by machines); and (3) well-written specific comments are greatly appreciated
+by those who could be reading your code months/years later.
 
+We tried to start by figuring out what the tests were testing, hoping to understand the very purpose of the
+program.  Our guide instead pushed us to start by simply reading and properly indenting the existing code to
+see what we got from it.  Two ways to skin a cat.  
+
+I wish we could rewrite the driver code in a way that tested by numbers/data rather than by printed strings.  
+Alternatively, I wish all of the strings in the test had been written consistently.  This would have allowed
+us to refactor a couple of different blocks of code.  First, if we did not have to return the shorter string
+(see the tests in lines 57 and 59), we could have removed lines 21-25--the logic here is not necessary since
+it is already covered by lines 34-35.  We also could have converted the return string in line 44 to a loop,
+iterating through each array of the hash.  That actually would have been a case of replacing one line of code
+with about five lines, but it would have made the code much more adaptable, especially if the hash of foods
+were extended to four, five, or one hundred different types of foods.  A lesson in simplicity (solving the 
+immediate problem) versus flexibility (foreseeing likely future problems).
+=end
 
 
