494ca5c2bd4a47e0b0a2874fbc0542b32fa0ac42
Alan Florendo <alan.florendo@gmail.com>
2014-05-29 12:29:53 -0500

    6. solution

--- week_6/3_BoggleBoard/my_solution.rb
+++ week_6/3_BoggleBoard/my_solution.rb
@@ -1,7 +1,7 @@
 # U2.W6: Create a BoggleBoard Class
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge by myself.
 
 
 # 2. Pseudocode
@@ -9,9 +9,35 @@
 
 
 # 3. Initial Solution
+=begin
 class BoggleBoard
  
   #your code here
+    def initialize(board)    
+        if board.empty?     
+            raise ArgumentError.new("Board cannot be empty")
+        else
+            @board = board
+        end
+    end
+
+  def create_word(*coords)
+    coords.map { |coord| @board[coord.first][coord.last]}.join("")
+  end
+  
+  def get_row(row)
+    @board[row]
+  end
+
+
+  def get_col(col)
+    col_array = []
+    num_cols = @board.length # #cols equals #row_arrays in the board array
+    num_cols.times do |looping_row| 
+      col_array << @board[looping_row][col]
+    end
+    return col_array
+  end
  
 end
  
@@ -23,18 +49,90 @@ dice_grid = [["b", "r", "a", "e"],
  
 boggle_board = BoggleBoard.new(dice_grid)
  
-
+=end
 
 # 4. Refactored Solution
 
+class BoggleBoard
+ 
+  #your code here
+    def initialize(board)    
+        if board.empty?     
+            raise ArgumentError.new("Board cannot be empty")
+        else
+            @board = board
+        end
+    end
+
+  def create_word(*coords)
+    coords.map { |coord| @board[coord.first][coord.last]}.join("")
+  end
+  
+  def get_row(row)
+    @board[row]
+  end
 
 
+  def get_col(col)
+    col_array = []
+    num_cols = @board.length # #cols equals #row_arrays in the board array
+    num_cols.times do |looping_row| 
+      col_array << @board[looping_row][col]
+    end
+    return col_array
+  end
+
+  def get_diagonal(first, last)
+  	row = first[0]
+  	col = first[1]
+  	last_row = last[0]
+  	last_col = last[1]
+  	diag_word = []
+
+  	# Create slope variable for row and column
+  	# 1 or -1 (get slope to only 1 or -1 by dividing the signed difference by the abs/val difference)
+  	row_slope = (last_row - row)/((last_row - row).abs)
+  	col_slope = (last_col - col)/((last_col - col).abs)
+
+
+  	until row == last_row
+  		diag_word << @board[row][col]
+  		row = row + row_slope
+  		col = col + col_slope
+  	end
+
+  end
+ 
+end
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 # create driver test code to retrieve a value at a coordinate here:
 # implement tests for each of the methods here:
+
+def assert(test,false_msg="False")
+	if test
+		puts "True"
+	else
+		puts false_msg
+	end
+end
+
+# test 1:  spell out dock using create_word
+assert(boggle_board.create_word([1,2], [1,1], [2,1], [3,2]) == "dock", "Error:  coordinates should spell out 'dock'")
+
+# test 2: get_row tests
+assert(boggle_board.get_row(0)==["b","r","a","e"], "Error: row should spell out 'brae'")
+assert(boggle_board.get_row(1)==["i","o","d","t"], "Error: row should spell out 'iodt'")
+assert(boggle_board.get_row(2)==["e","c","l","r"], "Error: row should spell out 'eclr'")
+assert(boggle_board.get_row(3)==["t","a","k","e"], "Error: row should spell out 'take'")
+
+# test 3: get_col tests
+assert(boggle_board.get_col(0)==["b","i","e","t"], "Error: row should spell out 'biet'")
+assert(boggle_board.get_col(1)==["r","o","c","a"], "Error: row should spell out 'roca'")
+assert(boggle_board.get_col(2)==["a","d","l","k"], "Error: row should spell out 'adlk'")
+assert(boggle_board.get_col(3)==["e","t","r","e"], "Error: row should spell out 'etre'")
 
 
 # 5. Reflection 
--- week_6/4_playlist_solo_challenge/my_solution.rb
+++ week_6/4_playlist_solo_challenge/my_solution.rb
@@ -1,20 +1,163 @@
 # U2.W6: Create a Playlist from Driver Code
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this solo challenge by myself.
 
 
 # Pseudocode
+=begin
 
+Error 1 (uninitialized constant Song):  add Song class
+Error 2 (uninitialized constant Playlist):  add Playlist class
+Error 3 (undefined method add for Playlist class):  add add method to Playlist class
+	add a song to the songs array
+Error 4 (undefined method num_of_tracks for Playlist class):  
+	add num_of_tracks method to Playlist class
+	return the array length of the num_of_tracks array
+Error 5 (undefined method play for Song class):  
+	add play method to Song class
+	use strings to "play" the song
+Error 6 (undefined method remove for Playlist class):  
+	add remove method to Playlist class
+	loop through each title to see if there is a match
+	if there is a match,
+		delete the song
+		move each remaining song back one track (deleting the last (duplicate) track)
+Error 7 (undefined method includes? for Playlist class):  
+	add includes? method to Playlist class
+Error 8 (undefined method play_all for Playlist class):  
+	add play_all method to Playlist class
+	for each song in the songs array, print the song title
+
+=end
 
 
 # Initial Solution
+=begin
+class Song
 
+	def initialize(title, album)
+		@title = title
+		@album = album
+	end
 
+	def play
+		puts "playing #{@title}..."
+		puts "end of song"
+	end
+
+	def title
+		@title
+	end
+end
+
+class Playlist
+	def initialize(*song)
+		@songs = song
+	end
+
+	def add(*song)
+		song.each { |title| @songs << title}
+	end
+
+	def num_of_tracks
+		@songs.length
+	end
+
+	def remove(song_title)
+		index = 0
+		@songs.each do |element|
+			if element.title == song_title.title
+				until index == @songs.length 
+					@songs[index] = @songs[index+1]
+					index += 1
+					puts "#{song_title.title} has been removed"
+				end
+				@songs.pop(1)
+			else
+				index += 1
+			end
+		end
+	end
+
+	def includes?(title)
+		song_included = false
+		@songs.each do |song|
+			if song == title
+				song_included = true
+			end
+		end
+		return song_included
+	end
+
+	def play_all
+		@songs.each do |song|
+			print "playing #{song.title}..."
+			puts "end of song"
+		end
+	end
+
+	def display
+		puts "\n     Playlist     "
+		puts "******************"
+		@songs.each do |song|
+			puts "#{song.title}"
+		end
+	end
+end
+
+=end
 
 
 # Refactored Solution
 
+class Song
+	attr_reader :title, :album
+
+	def initialize(title, album)
+		@title = title
+		@album = album
+	end
+
+	def play
+		puts "playing #{@title}..."
+		puts "end of song"
+	end
+end
+
+class Playlist
+	def initialize(*song)
+		@songs = song
+	end
+
+	def add(*song)
+		song.each { |title| @songs << title}
+	end
+
+	def num_of_tracks
+		@songs.length
+	end
+
+	def remove(song_title)
+		@songs.delete(song_title)
+	end
+
+	def includes?(song_title)
+		@songs.include?(song_title)
+	end
+
+	def play_all
+		@songs.each { |song| song.play}
+	end
+
+	def display
+		puts "\n     Playlist     "
+		puts "******************"
+		@songs.each do |song|
+			puts "#{song.title}"
+		end
+	end
+end
 
 
 
@@ -24,22 +167,45 @@
 one_by_one = Song.new("One by One", "Sirenia")
 world_so_cold = Song.new("World So Cold", "Three Days Grace") 
 going_under = Song.new("Going Under", "Evanescence")
- 
+
+
 my_playlist = Playlist.new(one_by_one, world_so_cold, going_under)
  
 lying_from_you = Song.new("Lying From You", "Linkin Park")
 angels = Song.new("Angels", "Within Temptation")
- 
+
 my_playlist.add(lying_from_you, angels)
 p my_playlist.num_of_tracks == 5
 going_under.play
 my_playlist.remove(angels)
 p my_playlist.includes?(lying_from_you) == true
+p my_playlist.num_of_tracks == 4
 my_playlist.play_all
 my_playlist.display
 
 
 
 
-
 # Reflection 
+=begin
+I really enjoyed this challenge.  Often I don't find the working by error-driven development very 
+educational; I feel like I'm just going through a series of steps and being expressly told how to 
+fix things.  That wasn't so much the case here.  The first few errors were easy; no classes or 
+methods existed, so I knew I had to add them.  But then things got more complex--the answers weren't 
+so obvious.  The error message made it clear that I needed to add a method, but it wasn't so clear 
+exactly how I'd bring the necessary functionality to that method.  
+
+I think this exercise really helped to get me more comfortable with classes (I was going to say that 
+it solidified my understanding of classes, but that may have been a bit much.)  I feel that I get the 
+general principles and, importantly, I can use trial and error to get my code to work.  I consistently 
+get hung up on whether I need to use an attribute of an object or simply the name of the object 
+itself--e.g., using song instead of song.title and vice-versa.  For the moment, I've used trial and 
+error to solve this issue, but I know I need to develop more instinct regarding this issue.
+
+I did my initial solution without consulting any outside resources.  I knew there must be easier ways 
+to delete a song from a playlist array or to use include? to determine whether a song was included 
+in the playlist.  I thought it would be good practice for me to deal with the inner logic of these 
+two ideas using more basic control flow techniques (if, else, loops, etc.).  If I had done more 
+research for these methods in my initial solution, I am not sure I would have been able to come up 
+with any ideas for refactoring.
+=end
